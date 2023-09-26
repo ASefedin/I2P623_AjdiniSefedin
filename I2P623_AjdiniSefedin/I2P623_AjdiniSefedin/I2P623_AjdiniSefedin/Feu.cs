6fa5e8c3b48005241934233c7f6f5a39f1e7beef
ASefedin <115707837+ASefedin@users.noreply.github.com>
2023-09-26 14:27:50 +0200

    Add files via upload

---
+++ I2P623_AjdiniSefedin/I2P623_AjdiniSefedin/I2P623_AjdiniSefedin/Feu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P623_AjdiniSefedin
+{
+    internal class Feu
+    {
+        private string _couleur;
+        private string _identifiant;
+        private string _couleur1 = " Orange";
+        private string _couleur2 = " Vert";
+        
+        public string Identifiant
+        {
+            get { return _identifiant; }
+            set { _identifiant = value; }
+        }
+        public string Couleur
+        {
+            get { return _couleur; }
+            set { _couleur = value; }
+        }
+
+
+        public Feu(string identifiant, string couleur)
+    {
+        _identifiant = identifiant;
+        _couleur = couleur;
+    }
+
+    public string AfficheEtat()
+    {
+        string liste = "Le feu de signalisation " + _identifiant + " est" + _couleur ;
+        return liste;
+    }
+
+    public string AfficheChangementEtatOrange()
+        {
+            string liste = "Le feu de signalisation " + _identifiant + " est " + _couleur1 ;
+            return liste;
+            
+        }
+        public string AfficheChangementEtatVert()
+        {
+            string liste = "Le feu de signalisation " + _identifiant + " est " + _couleur2 ;
+            return liste;
+        }
+
+        public string AfficheChangementEtat()
+        {
+            string liste = "Faire passer le " + _identifiant + " est " + _couleur1 ;
+            return liste;
+        }
+        public string ClignotantEteint()
+        {
+            string liste =_identifiant + " est éteint" ;
+            return liste;
+        }
+        
+        public string ClignotantAllume()
+        {
+            string liste =_identifiant + " est allumé" ;
+            return liste;
+        }
+    }
+    
+}
---
+++ I2P623_AjdiniSefedin/I2P623_AjdiniSefedin/I2P623_AjdiniSefedin/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using I2P623_AjdiniSefedin;
+using System.Net.Security;
+
+Console.WriteLine("Etat des feux :");
+Console.WriteLine("---------------");
+
+Feu FeuRouge = new Feu(" 1001", " Rouge");
+Feu Feu2 = new Feu("007", " Vert");
+
+
+Console.WriteLine(FeuRouge.AfficheEtat());
+Console.WriteLine(Feu2.AfficheEtat());
+
+Console.WriteLine();
+Console.WriteLine("Changement d'état :");
+Console.WriteLine("-------------------");
+
+if (FeuRouge.Couleur == " Rouge")
+{
+    for (int i = 0; i < 2; i++)
+    {
+        Console.WriteLine(FeuRouge.AfficheChangementEtatVert());
+        Console.WriteLine(FeuRouge.AfficheChangementEtatOrange());
+        Console.WriteLine(FeuRouge.AfficheEtat());
+    }
+}
+else if (FeuRouge.Couleur == " Orange")
+{
+    for (int i = 0; i < 2; i++)
+    {
+        Console.WriteLine(FeuRouge.AfficheEtat());
+        Console.WriteLine(FeuRouge.AfficheChangementEtatVert());
+        Console.WriteLine(FeuRouge.AfficheChangementEtatOrange());
+    }
+}
+else
+{
+    for (int i = 0; i < 2; i++)
+    {
+        Console.WriteLine(FeuRouge.AfficheChangementEtatOrange());
+        Console.WriteLine(FeuRouge.AfficheEtat());
+        Console.WriteLine(FeuRouge.AfficheChangementEtatVert());
+    }
+}
+
+
+Console.WriteLine();
+Console.WriteLine(Feu2.AfficheChangementEtat());
+Console.WriteLine(Feu2.AfficheChangementEtatOrange());
+Console.WriteLine("----------------------------------");
+Console.WriteLine();
+
+Console.WriteLine("Feu clignotant :");
+Console.WriteLine("---------------");
+
+for (int i = 0; i < 3; i++)
+{
+    if (Feu2.Couleur == " Vert")
+    {
+        Console.WriteLine(Feu2.ClignotantAllume());
+        Console.WriteLine(Feu2.ClignotantEteint());
+    }
+    else
+    {
+        Console.WriteLine(Feu2.ClignotantEteint());
+        Console.WriteLine(Feu2.ClignotantAllume());
+    }
+}
